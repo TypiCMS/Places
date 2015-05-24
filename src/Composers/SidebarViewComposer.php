08eb422bb4bc686ba015b051e6ebdc012be583e3
Samuel De Backer <sdebacker@gmail.com>
2015-05-24 22:42:43 +0200

    …

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -17,7 +17,7 @@ class SidebarViewComposer extends BaseSidebarViewComposer
                 $item->route('admin.places.index');
                 $item->append('admin.places.create');
                 $item->authorize(
-                    $this->user->hasAccess('places.index')
+                    $this->auth->hasAccess('places.index')
                 );
             });
         });
