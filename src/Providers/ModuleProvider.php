1d5547fe9d6410e120f8135665623a34ae818add
Samuel De Backer <sdebacker@gmail.com>
2015-05-24 14:50:03 +0200

    tests publishing removed

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -35,9 +35,6 @@ class ModuleProvider extends ServiceProvider
             __DIR__ . '/../database' => base_path('database'),
         ], 'migrations');
         $this->publishes([
-            __DIR__ . '/../../tests' => base_path('tests'),
-        ], 'tests');
-        $this->publishes([
             __DIR__ . '/../../resources/assets' => public_path(),
         ], 'scripts');
 
