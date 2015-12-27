30058472a2126dcb5c5458a8e1dc177b3ee5e217
Samuel De Backer <sdebacker@gmail.com>
2015-12-27 23:07:56 +0100

    thumbUrl

--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', $model->title . ' – ' . trans('news::global.name') . ' – ' . $websiteTitle)
 @section('ogTitle', $model->title)
 @section('description', $model->summary)
-@section('image', $model->present()->thumbAbsoluteSrc())
+@section('image', $model->present()->thumbUrl())
 @section('bodyClass', 'body-places body-place-' . $model->id . ' body-page body-page-' . $page->id)
 
 @section('js')
