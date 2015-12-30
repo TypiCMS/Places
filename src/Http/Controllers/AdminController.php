6ea9e7a791161dab166067aa93d9c4de0a94b657
Samuel De Backer <sdebacker@gmail.com>
2015-12-30 16:44:11 +0100

    admin controller docblocks

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace TypiCMS\Modules\Places\Http\Controllers;
 
 use TypiCMS\Modules\Core\Http\Controllers\BaseAdminController;
 use TypiCMS\Modules\Places\Http\Requests\FormRequest;
+use TypiCMS\Modules\Places\Models\Place;
 use TypiCMS\Modules\Places\Repositories\PlaceInterface;
 
 class AdminController extends BaseAdminController
@@ -16,9 +17,9 @@ class AdminController extends BaseAdminController
     /**
      * Store a newly created resource in storage.
      *
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Places\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(FormRequest $request)
     {
@@ -30,15 +31,15 @@ class AdminController extends BaseAdminController
     /**
      * Update the specified resource in storage.
      *
-     * @param  $model
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Places\Models\Place              $place
+     * @param \TypiCMS\Modules\Places\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update($model, FormRequest $request)
+    public function update(Place $place, FormRequest $request)
     {
         $this->repository->update($request->all());
 
-        return $this->redirect($request, $model);
+        return $this->redirect($request, $place);
     }
 }
