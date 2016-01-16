83e562c5fd4811e9a9853cbb2c6a04a78fa62f92
Samuel De Backer <sdebacker@gmail.com>
2016-01-16 23:24:29 +0100

    lang-switcher tpl

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -5,7 +5,9 @@
         <span>@{{ models.length }} @choice('places::global.places', 2)</span>
     </h1>
 
-    @include('core::admin._tabs-lang-list')
+    <div class="btn-toolbar">
+        @include('core::admin._lang-switcher')
+    </div>
 
     <div class="table-responsive">
 
