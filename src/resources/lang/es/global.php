4a06f2074f29978485f6e4ac81f366a53135eb46
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 12:41:02 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name'         => 'Lugares',
+    'places'       => 'lugar|lugares',
+    'New'          => 'Nuevo lugar',
+    'Edit'         => 'Editar lugar',
+    'Back'         => 'Volver a los lugares',
+    'Find nearest' => 'Encontrar dirección más cercana',
+    'address'      => 'direccion',
+    'Address'      => 'Dirección',
+    'Search'       => 'Buscar',
+    'Filter'       => 'Filtrar',
+];
