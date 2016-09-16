80815e53e39eeb72493c652a67f988e2a07ddaf5
Samuel De Backer <sdebacker@gmail.com>
2016-09-16 23:38:59 +0200

    missing use

--- src/Repositories/EloquentPlace.php
+++ src/Repositories/EloquentPlace.php
@@ -4,6 +4,7 @@ namespace TypiCMS\Modules\Places\Repositories;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Request;
+use TypiCMS\Modules\Core\EloquentRepository;
 use TypiCMS\Modules\Places\Models\Place;
 use stdClass;
 
