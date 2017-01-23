d6b9f89f3efac7fcb3189a4b445d8e6b2724fbfb
Samuel De Backer <sdebacker@gmail.com>
2017-01-23 19:56:08 +0100

    Front office routes name

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -34,8 +34,8 @@ class RouteServiceProvider extends ServiceProvider
                 $options = $page->private ? ['middleware' => 'auth'] : [];
                 foreach (locales() as $lang) {
                     if ($page->translate('status', $lang) && $uri = $page->uri($lang)) {
-                        $router->get($uri, $options + ['as' => $lang.'.places', 'uses' => 'PublicController@index']);
-                        $router->get($uri.'/{slug}', $options + ['as' => $lang.'.places.slug', 'uses' => 'PublicController@show']);
+                        $router->get($uri, $options + ['uses' => 'PublicController@index'])->name($lang.'::index-places');
+                        $router->get($uri.'/{slug}', $options + ['uses' => 'PublicController@show'])->name($lang.'::place');
                     }
                 }
             }
--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -2,7 +2,7 @@
     @if ($place->latitude && $place->longitude)
     <a class="btn-map" href="" title="{{ __('db.Show on map') }}"><i class="fa fa-map-marker"></i><span class="sr-only">{{ __('db.Show on map') }}</span></a>
     @endif
-    <a href="{{ route($lang.'.places.slug', array($place->slug)) }}" title="{{ __('db.More') }}">
+    <a href="{{ route($lang.'::place', array($place->slug)) }}" title="{{ __('db.More') }}">
         {{ $place->title }}
     </a>
 </li>
