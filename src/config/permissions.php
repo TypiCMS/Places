0e82c444c4f67389bb18142721869f381fd1d89d
Samuel De Backer <sdebacker@gmail.com>
2017-04-18 23:50:18 +0200

    Simpler permissions

--- src/config/permissions.php
+++ src/config/permissions.php
@@ -2,11 +2,9 @@
 
 return [
     'places' => [
-        'index-places' => 'Show list',
+        'index-places' => 'View list',
         'create-place' => 'Create',
-        'store-place' => 'Store',
         'edit-place' => 'Edit',
-        'update-place' => 'Update',
         'destroy-place' => 'Destroy',
     ],
 ];
