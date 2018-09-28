0e4c0e6932f21849f68d37d1bd17ae6cb3a71df9
Samuel De Backer <sdebacker@gmail.com>
2018-09-28 17:31:34 +0200

    header markup

--- src/resources/views/admin/create.blade.php
+++ src/resources/views/admin/create.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-    @include('core::admin._button-back', ['module' => 'places'])
-    <h1>
-        @lang('New place')
-    </h1>
+    <div class="header">
+        @include('core::admin._button-back', ['module' => 'places'])
+        <h1 class="header-title">@lang('New place')</h1>
+    </div>
 
     {!! BootForm::open()->action(route('admin::index-places'))->multipart()->role('form') !!}
         @include('places::admin._form')
