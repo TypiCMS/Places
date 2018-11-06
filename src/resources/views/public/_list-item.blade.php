723451464e9bb05560466f2c6e459ca8f640417f
Samuel De Backer <sdebacker@gmail.com>
2018-11-06 17:41:05 +0100

    no need of an array

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -2,7 +2,7 @@
     @if ($place->latitude && $place->longitude)
     <a class="places-item-btn-map" href="" title="{{ __('db.Show on map') }}"><i class="fa fa-map-marker"></i><span class="sr-only">{{ __('db.Show on map') }}</span></a>
     @endif
-    <a class="places-item-link" href="{{ route($lang.'::place', array($place->slug)) }}" title="@lang('db.More')">
+    <a class="places-item-link" href="{{ route($lang.'::place', $place->slug) }}" title="@lang('db.More')">
         {{ $place->title }}
     </a>
 </li>
