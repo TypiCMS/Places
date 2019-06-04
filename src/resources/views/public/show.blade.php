8ac4d7987c6e2f028bc0ba7b7cb211da551f1ed9
Samuel De Backer <sdebacker@gmail.com>
2019-06-04 14:21:55 +0200

    Update show.blade.php

--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -37,10 +37,10 @@
         </p>
         <p class="place-info">
             @if ($model->info)
-                {{ nl2br($model->info) }}
+                {!! nl2br($model->info) !!}
             @endif
         </p>
-        <p class="place-summary">{{ nl2br($model->summary) }}</p>
+        <p class="place-summary">{!! nl2br($model->summary) !!}</p>
         <div class="place-body">{!! $model->present()->body !!}</div>
         @include('files::public._documents')
         @include('files::public._images')
