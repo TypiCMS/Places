c429860218c42b998e27b6c818394e8178efa8e1
Samuel De Backer <sdebacker@gmail.com>
2019-11-29 10:48:19 +0100

    slug required_with title validation rule

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -13,7 +13,7 @@ class FormRequest extends AbstractFormRequest
             'website' => 'nullable|url|max:255',
             'image_id' => 'nullable|integer',
             'title.*' => 'nullable|max:255',
-            'slug.*' => 'nullable|alpha_dash|max:255',
+            'slug.*' => 'nullable|alpha_dash|max:255|required_with:title.*',
         ];
     }
 }
