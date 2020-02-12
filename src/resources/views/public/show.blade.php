3e9f6d3b975028b607239b4fc7824c930a3f995e
Samuel De Backer <sdebacker@gmail.com>
2020-02-12 17:01:25 +0100

    @isset in place of @if

--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -18,33 +18,35 @@
 
     <div class="place">
         <h1 class="place-title">{{ $model->title }}</h1>
-        @if ($model->latitude && $model->longitude)
+        @isset($model->latitude, $model->longitude)
         <div class="place-map" id="map" data-url="/{{ $lang }}/places-json/{{ $model->id }}" style="height: 500px"></div>
-        @endif
+        @endisset
         @isset($model->image)
         <img class="place-image" src="{!! $model->present()->image(null, 1000) !!}" alt="">
         @endisset
-        <p class="place-contact" id="place" data-id="{{ $model->id }}">
-            @if ($model->address)
-                {{ $model->address }}<br>
+        <div class="place-contact">
+            @isset($model->address)
+            <address>{{ $model->address }}</address>
             @endif
-            @if ($model->phone)
-                {{ $model->phone }}<br>
+            @isset($model->phone)
+            <p class="place-phone">{{ $model->phone }}</p>
             @endif
-            @if ($model->email)
-                <a href="mailto:{{ $model->email }}">{{ $model->email }}</a><br>
+            @isset($model->email)
+            <p class="place-email"><a href="mailto:{{ $model->email }}">{{ $model->email }}</a></p>
             @endif
-            @if ($model->website)
-                <a href="{{ $model->website }}">{{ $model->website }}</a><br>
+            @isset($model->website)
+            <p class="place-website"><a href="{{ $model->website }}">{{ parse_url($model->website, PHP_URL_HOST) }}</a></p>
             @endif
-        </p>
-        <p class="place-info">
-            @if ($model->info)
-                {!! nl2br($model->info) !!}
-            @endif
-        </p>
+        </div>
+        @isset($model->info)
+        <p class="place-info">{!! nl2br($model->info) !!}</p>
+        @endisset
+        @isset($model->summary)
         <p class="place-summary">{!! nl2br($model->summary) !!}</p>
+        @endisset
+        @isset($model->body)
         <div class="place-body">{!! $model->present()->body !!}</div>
+        @endisset
         @include('files::public._documents')
         @include('files::public._images')
     </div>
