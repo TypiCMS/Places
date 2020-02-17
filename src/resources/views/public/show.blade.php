072eeead0f14866c3b18bd5040a43f3a28432103
Samuel De Backer <sdebacker@gmail.com>
2020-02-17 11:27:45 +0100

    @empty in place of @isset

--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -18,35 +18,35 @@
 
     <div class="place">
         <h1 class="place-title">{{ $model->title }}</h1>
-        @isset($model->latitude, $model->longitude)
+        @empty(!$model->latitude, $model->longitude)
         <div class="place-map" id="map" data-url="/{{ $lang }}/places-json/{{ $model->id }}" style="height: 500px"></div>
-        @endisset
-        @isset($model->image)
+        @endempty
+        @empty(!$model->image)
         <img class="place-image" src="{!! $model->present()->image(null, 1000) !!}" alt="">
-        @endisset
+        @endempty
         <div class="place-contact">
-            @isset($model->address)
+            @empty(!$model->address)
             <address>{{ $model->address }}</address>
-            @endif
-            @isset($model->phone)
+            @endempty
+            @empty(!$model->phone)
             <p class="place-phone">{{ $model->phone }}</p>
-            @endif
-            @isset($model->email)
+            @endempty
+            @empty(!$model->email)
             <p class="place-email"><a href="mailto:{{ $model->email }}">{{ $model->email }}</a></p>
-            @endif
-            @isset($model->website)
+            @endempty
+            @empty(!$model->website)
             <p class="place-website"><a href="{{ $model->website }}">{{ parse_url($model->website, PHP_URL_HOST) }}</a></p>
-            @endif
+            @endempty
         </div>
-        @isset($model->info)
+        @empty(!$model->info)
         <p class="place-info">{!! nl2br($model->info) !!}</p>
-        @endisset
-        @isset($model->summary)
+        @endempty
+        @empty(!$model->summary)
         <p class="place-summary">{!! nl2br($model->summary) !!}</p>
-        @endisset
-        @isset($model->body)
+        @endempty
+        @empty(!$model->body)
         <div class="place-body">{!! $model->present()->body !!}</div>
-        @endisset
+        @endempty
         @include('files::public._documents')
         @include('files::public._images')
     </div>
