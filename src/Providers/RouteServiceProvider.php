dfd537a7f1bf12f7cd51439df102d354a2ae1fd4
Samuel De Backer <sdebacker@gmail.com>
2020-09-18 17:51:50 +0200

    Update RouteServiceProvider.php

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -22,14 +22,14 @@ class RouteServiceProvider extends ServiceProvider
              * Front office routes
              */
             if ($page = TypiCMS::getPageLinkedToModule('places')) {
-                $router->middleware('public')->group(function (Router $router) use ($page) {
-                    $options = $page->private ? ['middleware' => 'auth'] : [];
+                $middleware = $page->private ? ['public', 'auth'] : ['public'];
+                $router->middleware($middleware)->group(function (Router $router) use ($page) {
                     foreach (locales() as $lang) {
                         if ($page->translate('status', $lang) && $uri = $page->uri($lang)) {
-                            $router->get($uri, $options + ['uses' => [PublicController::class, 'index']])->name($lang.'::index-places');
-                            $router->get($lang.'/places-json', $options + ['uses' => [PublicController::class, 'json']]);
-                            $router->get($lang.'/places-json/{id}', $options + ['uses' => [PublicController::class, 'jsonItem']]);
-                            $router->get($uri.'/{slug}', $options + ['uses' => [PublicController::class, 'show']])->name($lang.'::place');
+                            $router->get($uri, [PublicController::class, 'index'])->name($lang.'::index-places');
+                            $router->get($lang.'/places-json', [PublicController::class, 'json']);
+                            $router->get($lang.'/places-json/{id}', [PublicController::class, 'jsonItem']);
+                            $router->get($uri.'/{slug}', [PublicController::class, 'show'])->name($lang.'::place');
                         }
                     }
                 });
