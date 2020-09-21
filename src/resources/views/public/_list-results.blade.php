859d209cee86745b6b1d57ac57cc247f01081d2d
Samuel De Backer <sdebacker@gmail.com>
2020-09-21 22:39:48 +0200

    Create _list-results.blade.php

---
+++ src/resources/views/public/_list-results.blade.php
@@ -0,0 +1,9 @@
+<ul class="place-list-results-list">
+    @foreach ($items as $place)
+    <li class="place-list-results-item">
+        <a class="place-list-results-item-link" href="{{ $place->uri() }}">
+            {{ $place->title }}
+        </a>
+    </li>
+    @endforeach
+</ul>
