55d4956dcf3d3bc4e940e699b3020bdbfce1e28f
Samuel De Backer <sdebacker@gmail.com>
2022-04-20 10:43:51 +0200

    Tabs removed

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -19,61 +19,39 @@
     <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
     <files-field :init-files="{{ $model->files }}"></files-field>
 
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link active" href="#tab-main" data-bs-toggle="tab">{{ __('Content') }}</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#tab-info" data-bs-toggle="tab">{{ __('Info') }}</a>
-        </li>
-    </ul>
-
-    <div class="tab-content">
-
-        {{-- Main tab --}}
-        <div class="tab-pane fade show active" id="tab-main">
-
-            @include('core::form._title-and-slug')
-            <div class="mb-3">
-                {!! TranslatableBootForm::hidden('status')->value(0) !!}
-                {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-            </div>
-            {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-            {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
-
-        </div>
-
-        {{-- Info tab --}}
-        <div class="tab-pane fade" id="tab-info">
-
-            {!! BootForm::textarea(__('Address'), 'address')->rows(4) !!}
-
-            <div class="row gx-3">
-                <div class="col-sm-6">
-                    {!! BootForm::email(__('Email'), 'email') !!}
-                </div>
-                <div class="col-sm-6">
-                    {!! BootForm::text(__('Website'), 'website')->placeholder('https://') !!}
-                </div>
-            </div>
-
-            <div class="row gx-3">
-                <div class="col-sm-6">
-                    {!! BootForm::text(__('Phone'), 'phone') !!}
-                </div>
-            </div>
-
-            <div class="row gx-3">
-                <div class="col-sm-6">
-                    {!! BootForm::text(__('Latitude'), 'latitude') !!}
-                </div>
-                <div class="col-sm-6">
-                    {!! BootForm::text(__('Longitude'), 'longitude') !!}
-                </div>
-            </div>
-
-        </div>
-
+    @include('core::form._title-and-slug')
+    <div class="mb-3">
+        {!! TranslatableBootForm::hidden('status')->value(0) !!}
+        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
     </div>
+
+    <div class="row gx-3">
+        <div class="col-sm-6">
+            {!! BootForm::email(__('Email'), 'email') !!}
+        </div>
+        <div class="col-sm-6">
+            {!! BootForm::text(__('Website'), 'website')->placeholder('https://') !!}
+        </div>
+    </div>
+
+    <div class="row gx-3">
+        <div class="col-sm-6">
+            {!! BootForm::text(__('Phone'), 'phone') !!}
+        </div>
+    </div>
+
+    {!! BootForm::textarea(__('Address'), 'address')->rows(4) !!}
+
+    <div class="row gx-3">
+        <div class="col-sm-6">
+            {!! BootForm::text(__('Latitude'), 'latitude') !!}
+        </div>
+        <div class="col-sm-6">
+            {!! BootForm::text(__('Longitude'), 'longitude') !!}
+        </div>
+    </div>
+
+    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+    {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
 
 </div>
