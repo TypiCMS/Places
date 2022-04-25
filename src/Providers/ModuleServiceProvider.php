c28e0ba6eb133ad5c42e515697dbc98114fc4c3c
Samuel De Backer <sdebacker@gmail.com>
2022-04-25 16:37:34 +0200

    config

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -15,7 +15,7 @@ class ModuleServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'typicms.modules');
+        $this->mergeConfigFrom(__DIR__.'/../config/places.php', 'typicms.modules.places');
 
         $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'places');
 
