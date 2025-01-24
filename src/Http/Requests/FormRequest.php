ce863afa0d0040611042bcfbf3208d75a1017c0f
Samuel De Backer <sdebacker@gmail.com>
2025-01-24 15:25:33 +0100

    Maximum number of characters increased.

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -15,7 +15,7 @@ class FormRequest extends AbstractFormRequest
             'slug.*' => 'nullable|alpha_dash|max:255|required_if:status.*,1|required_with:title.*',
             'status.*' => 'boolean',
             'summary.*' => 'nullable|max:1000',
-            'body.*' => 'nullable|max:10000',
+            'body.*' => 'nullable|max:20000',
             'address' => 'nullable|max:1000',
             'email' => 'nullable|email:rfc,dns|max:255',
             'website' => 'nullable|url|max:255',
