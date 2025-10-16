c50e594ee57abbb53fa0e1b16c0aa505b8169524
Samuel De Backer <sdebacker@gmail.com>
2025-10-16 13:06:08 +0200

    Update _form.blade.php

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -45,51 +45,58 @@
 <div class="content">
     <x-core::form-errors />
 
-    <file-manager></file-manager>
-    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-    <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
-    <files-field :init-files="{{ $model->files }}"></files-field>
-
-    <x-core::title-and-slug-fields :locales="locales()" />
-    <div class="mb-3">
-        {!! TranslatableBootForm::hidden('status')->value(0) !!}
-        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-    </div>
-
-    <div class="row gx-3">
-        <div class="col-sm-6">
-            {!! BootForm::email(__('Email'), 'email')->autocomplete('off') !!}
-        </div>
-        <div class="col-sm-6">
-            {!! BootForm::text(__('Website'), 'website')->placeholder('https://') !!}
-        </div>
-    </div>
-
-    <div class="row gx-3">
-        <div class="col-sm-6">
-            {!! BootForm::text(__('Phone'), 'phone')->autocomplete('off') !!}
-        </div>
-    </div>
-
-    {!! BootForm::textarea(__('Address'), 'address')->rows(4)->autocomplete('off') !!}
-
-    <div class="row gx-3">
-        <div class="col-md-5">
-            {!! BootForm::text(__('Latitude'), 'latitude') !!}
-        </div>
-        <div class="col-md-5">
-            {!! BootForm::text(__('Longitude'), 'longitude') !!}
-        </div>
-        <div class="col-md-2">
+    <div class="row">
+        <div class="col-lg-8">
+            <x-core::title-and-slug-fields :locales="locales()" />
             <div class="mb-3">
-                <label class="form-label" for="geocode-button">&nbsp;</label>
-                <p class="mb-0">
-                    <button class="btn btn-secondary w-100" id="geocode-button" type="button">Chercher</button>
-                </p>
+                {!! TranslatableBootForm::hidden('status')->value(0) !!}
+                {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+            </div>
+
+            <div class="row gx-3">
+                <div class="col-sm-6">
+                    {!! BootForm::email(__('Email'), 'email')->autocomplete('off') !!}
+                </div>
+                <div class="col-sm-6">
+                    {!! BootForm::text(__('Website'), 'website')->placeholder('https://') !!}
+                </div>
+            </div>
+
+            <div class="row gx-3">
+                <div class="col-sm-6">
+                    {!! BootForm::text(__('Phone'), 'phone')->autocomplete('off') !!}
+                </div>
+            </div>
+
+            {!! BootForm::textarea(__('Address'), 'address')->rows(4)->autocomplete('off') !!}
+
+            <div class="row gx-3">
+                <div class="col-md-5">
+                    {!! BootForm::text(__('Latitude'), 'latitude') !!}
+                </div>
+                <div class="col-md-5">
+                    {!! BootForm::text(__('Longitude'), 'longitude') !!}
+                </div>
+                <div class="col-md-2">
+                    <div class="mb-3">
+                        <label class="form-label" for="geocode-button">&nbsp;</label>
+                        <p class="mb-0">
+                            <button class="btn btn-secondary w-100" id="geocode-button" type="button">Chercher</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+            <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
+        </div>
+        <div class="col-lg-4">
+            <div class="right-column">
+                <file-manager></file-manager>
+                <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+                <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
+                <files-field :init-files="{{ $model->files }}"></files-field>
             </div>
         </div>
     </div>
-
-    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-    <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
 </div>
