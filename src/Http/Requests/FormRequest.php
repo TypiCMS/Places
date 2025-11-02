7e6054925b9b072559c76235a181f827cd31abf8
Samuel De Backer <sdebacker@gmail.com>
2025-11-02 20:25:13 +0100

    Validation rules specified as arrays.

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -6,23 +6,23 @@ use TypiCMS\Modules\Core\Http\Requests\AbstractFormRequest;
 
 class FormRequest extends AbstractFormRequest
 {
-    /** @return array<string, string> */
+    /** @return array<string, list<string>> */
     public function rules(): array
     {
         return [
-            'image_id' => 'nullable|integer',
-            'og_image_id' => 'nullable|integer',
-            'title.*' => 'nullable|max:255',
-            'slug.*' => 'nullable|alpha_dash|max:255|required_if:status.*,1|required_with:title.*',
-            'status.*' => 'boolean',
-            'summary.*' => 'nullable|max:1000',
-            'body.*' => 'nullable|max:20000',
-            'address' => 'nullable|max:1000',
-            'email' => 'nullable|email:rfc,dns|max:255',
-            'website' => 'nullable|url|max:255',
-            'phone' => 'nullable|max:255',
-            'latitude' => 'nullable|numeric|between:-90,90',
-            'longitude' => 'nullable|numeric|between:-180,180',
+            'image_id' => ['nullable', 'integer'],
+            'og_image_id' => ['nullable', 'integer'],
+            'title.*' => ['nullable', 'max:255'],
+            'slug.*' => ['nullable', 'alpha_dash', 'max:255', 'required_if:status.*,1', 'required_with:title.*'],
+            'status.*' => ['boolean'],
+            'summary.*' => ['nullable', 'max:1000'],
+            'body.*' => ['nullable', 'max:20000'],
+            'address' => ['nullable', 'max:1000'],
+            'email' => ['nullable', 'email:rfc,dns', 'max:255'],
+            'website' => ['nullable', 'url', 'max:255'],
+            'phone' => ['nullable', 'max:255'],
+            'latitude' => ['nullable', 'numeric', 'between:-90,90'],
+            'longitude' => ['nullable', 'numeric', 'between:-180,180'],
         ];
     }
 }
