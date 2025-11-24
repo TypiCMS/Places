dafce76b36371d530b1d86e9fd7716d8e3cc2ca2
Samuel De Backer <sdebacker@gmail.com>
2025-11-24 11:12:09 +0100

    Make sidebar labels and groups configurable

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -14,10 +14,10 @@ class SidebarViewComposer
         if (Gate::denies('read places')) {
             return;
         }
-        $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+        $view->offsetGet('sidebar')->group(__(config('typicms.modules.places.sidebar.group', 'Content')), function (SidebarGroup $group): void {
             $group->id = 'content';
             $group->weight = 30;
-            $group->addItem(__('Places'), function (SidebarItem $item): void {
+            $group->addItem(__(config('typicms.modules.places.sidebar.label', 'Places')), function (SidebarItem $item): void {
                 $item->id = 'places';
                 $item->icon = config('typicms.modules.places.sidebar.icon');
                 $item->weight = config('typicms.modules.places.sidebar.weight');
